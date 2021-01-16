76b48284fc23e059baefaa76bf6ad7de12a460c0
Chii Law <cyee3012@hotmail.co.uk>
2021-01-16 13:48:36 +0900

    push

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/cocktails', to: 'cocktails#index'
-  get '/cocktails/:id', to: 'cocktails#show'
-  get '/cocktails/new', to: 'cocktails#new'
-  post '/cocktails', to: 'cocktails#create'
+  # get '/cocktails', to: 'cocktails#index'
+  # get '/cocktails/:id', to: 'cocktails#show'
+  # get '/cocktails/new', to: 'cocktails#new'
+  # post '/cocktails', to: 'cocktails#create'
+
+  resources :cocktails, only: [ :index, :show, :new, :create ]
 
 end
