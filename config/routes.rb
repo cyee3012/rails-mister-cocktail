cabf997be4699717a23bfaa000cb33207eafbd09
Chii Law <cyee3012@hotmail.co.uk>
2021-01-19 20:17:58 +0900

    destroy action works

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   # get '/cocktails/new', to: 'cocktails#new'
   # post '/cocktails', to: 'cocktails#create'
 
-  resources :cocktails, only: [:index, :show, :new, :create]
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
 
-  resources :doses, only: [:new, :create, :destroy]
+  resources :doses, only: :destroy
 
 end
