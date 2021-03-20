e4bf8028430ff4c45b71762ee372bc12d53d0450
Chii Law <cyee3012@hotmail.co.uk>
2021-03-20 23:34:11 +0900

    improved styling on dose form and cards

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -9,7 +9,7 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to cocktail_path(@cocktail)
+      redirect_to cocktail_path(@cocktail), success: "dose added!"
     else
       render :new
     end
